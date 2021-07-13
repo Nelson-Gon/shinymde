4205f9120371fc499259f59841a88b6688881797
Nelson-Gon <gonzabato@hotmail.com>
2021-07-13 20:24:33 +0300

    Init recoding, todo: figure out optional args

--- app.R
+++ app.R
@@ -10,16 +10,28 @@ library(mde)
      tabPanel("Summarise Missingness",
               dataTableOutput("summary_na"),
               fluidRow(
-                column(4, uiOutput("sort_by")),
-                column(4, selectInput("sort_order", "Sort Order",
+                column(2, uiOutput("sort_by")),
+                column(2, selectInput("sort_order", "Sort Order",
                                       choices=c("ascending", "descending"))),
-                column(4, uiOutput("group_by")),
-                column(4, uiOutput("exclude_columns"))
+                column(2, uiOutput("group_by")),
+                column(2, uiOutput("exclude_columns"))
                 
-              ))
+              )),
+     tabPanel("Recode Values",
+              dataTableOutput("recode_values"),
+              fluidRow(
+              column(2, selectInput("recode_type", "Kind of recoding",
+                                     choices = c("recode_as_na",
+                                                 "recode_na_as"),
+                                     selected="recode_na_as")),
+              # column(2, uiOutput("subset_cols")),
+              # column(2, textInput("pattern_type", "Pattern type", 
+              #                     value = NULL)),
+              # column(2, textInput("pattern", "Pattern", value=NULL)),
+              column(2, textInput("value_to_recode", "Value"))
      
-   )
- )
+   ))
+))
  
 server <- function(input, output, session){
   in_data <- reactive(
@@ -53,14 +65,39 @@ output$exclude_columns <- renderUI({
                         choices = names(in_data()),
                          multiple=TRUE)})
 
+output$subset_cols <- renderUI({selectInput("subset_cols",
+                                         "a subset to recode",
+                                       choices=names(in_data()),
+                                                 multiple=TRUE)})
+
 sort_order <- reactive({ifelse(input$sort_order=="descending", TRUE, FALSE)})   
 output$summary_na <- renderDataTable(na_summary(in_data(),
                                                 sort_by = input$sort_by,
-                                                grouping_cols = input$group_by,
+                                          grouping_cols = input$group_by,
                                    exclude_cols = input$exclude_columns,
                                    descending = sort_order()),
                               options = list(pageLength=5))
   
+# Create a switch to select recode_type
+
+recode_type <- reactive({
+  # optional arguments list 
+  # Filter only non NULL elements 
+  arguments = 
+    Filter(function(x) !is.null(x), list(df=in_data(),
+                value=input$value_to_recode))
+                #    pattern_type=input$pattern_type,
+                # pattern = input$pattern,
+                # subset_cols = input$subset_cols))
+ 
+  do.call(input$recode_type, arguments)
+})
+
+output$recode_values <- renderDataTable({
+recode_type()
+},
+options = list(pageLength=5)
+)
   
   
   
