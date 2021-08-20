4ea53494f289a8995f55482f25c49f9e627329bc
Nelson-Gon <gonzabato@hotmail.com>
2021-08-20 14:25:04 +0300

    Minimal startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,14 @@
-.onAttach <- .onAttach <- function(lib, pkg, ...){
-  
-  startup_msg <- paste0("Welcome to shinymde. This is shinymde version ",
-                        packageVersion("shinymde"),
-                        " using mde version ", packageVersion("mde"),
-                        ".\n",
-            "Please file issues and feedback at https://www.github.com/Nelson-Gon/shinymde/issues\n",
-            "Turn this message off using 'suppressPackageStartupMessages(library(mde))'\n",
-            "Happy Exploration :)")
-  packageStartupMessage(startup_msg)
+.onAttach <- .onAttach <- function(lib, pkg, ...) {
+  startup_msg <-
+    paste0(
+      "Welcome to shinymde. This is shinymde version ",
+      packageVersion("shinymde"),
+      " using mde version ",
+      packageVersion("mde"),
+      ".\n",
+"Please report issues at https://github.com/Nelson-Gon/shinymde/issues",
+"\nTurn this message off with suppressPackageStartupMessages(library(shinymde))",
+"\nHappy Exploration :)"
+)
+packageStartupMessage(startup_msg)
 }
