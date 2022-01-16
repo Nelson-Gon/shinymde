3e9ff777ddd8864d5745fbfb04832ee613fd5cf6
Nelson-Gon <gonzabato@hotmail.com>
2022-01-16 12:38:26 +0100

    Remove unnecessary code

--- R/drop_ui.R
+++ R/drop_ui.R
@@ -1,9 +1,9 @@
 drop_ui <- function() {
   tabItem(
   tabName = "drop_values",
-  div(
-    id = "drop_zone",
-      
+
+    sidebarLayout(
+      sidebarPanel(
         selectInput(
           "drop_type",
           "Kind of drop",
@@ -27,6 +27,7 @@ drop_ui <- function() {
           
           
         ),
+        
         fluidRow(
           column(9,selectInput(
             "group_by_drop",
@@ -37,12 +38,12 @@ drop_ui <- function() {
         fluidRow(
           
           two_columns(
-                 selectInput(
-                   "keep_columns_drop",
-                   "Keep Cols",
-                   choices = c("A", "B"),
-                   multiple = TRUE
-                 ),
+            selectInput(
+              "keep_columns_drop",
+              "Keep Cols",
+              choices = c("A", "B"),
+              multiple = TRUE
+            ),
             selectInput(
               "target_cols",
               "Target Cols",
@@ -53,34 +54,42 @@ drop_ui <- function() {
           
         ),
         fluidRow(
-          two_columns(
-          selectInput(
-            "pattern_type_drop",
-            "Pattern type",
-            choices = c("starts_with",
-                        "ends_with", "contains",
-                        "regex"),
-            selected = FALSE,
-            selectize = FALSE,
-            size = 4
-          ) ,
-          textInput("pattern_drop", "Pattern", value = NULL)
-        )),
-        br(),
+          column(8, 
+            selectInput(
+              "pattern_type_drop",
+              "Pattern type",
+              choices = c("starts_with",
+                          "ends_with", "contains",
+                          "regex"),
+              selected = FALSE,
+              selectize = FALSE,
+              size = 4
+            )) ,
+          column(4, 
+            textInput("pattern_drop", "Pattern", value = NULL)
+          )
+          ),
         
         shinyWidgets::downloadBttn(
           "downloadfile_drop",
-          "Download this report",
+          label = "",
+          size = "sm",
           style = "bordered",
           color = "default"
         )
         
+        
+        
       ),
-      div(id="drop_table",
-        shinycssloaders::withSpinner(dataTableOutput("drop_na"))
+      mainPanel(
+      
+    shinycssloaders::withSpinner(dataTableOutput("drop_na"))
+            
+        
         
       )
-    )
+    ))
+
     
     
     
